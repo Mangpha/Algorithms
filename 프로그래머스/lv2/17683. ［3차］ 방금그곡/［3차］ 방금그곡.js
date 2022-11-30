7e178378df8212b4cde0a707d59c24973f10eba1
Jeon Byeong Hee <78544031+Mangpha@users.noreply.github.com>
2022-11-30 14:20:15 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.15 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -0,0 +1,25 @@
+function solution(m, musicinfos) {
+	const HtoM = (s, e) => e[0] * 60 + Number(e[1]) - (s[0] * 60 + Number(s[1]));
+	const convM = (melody) =>
+		melody
+			.replaceAll('C#', 'c')
+			.replaceAll('D#', 'd')
+			.replaceAll('F#', 'f')
+			.replaceAll('G#', 'g')
+			.replaceAll('A#', 'a');
+	m = convM(m);
+	const dict = [];
+	musicinfos
+		.map((el) => el.split(','))
+		.forEach((music, idx) => {
+			const [start, end, title, mel] = music;
+			const time = HtoM(start.split(':'), end.split(':'));
+			const melody = convM(mel);
+			let cp = '';
+			for (let i = 0; i < time; i++) cp += melody[i % melody.length];
+			if (cp.includes(m)) dict.push({ time, title, idx });
+		});
+	return dict.length >= 1
+		? dict.sort((a, b) => b.time - a.time || a.idx - b.idx)[0].title
+		: '(None)';
+}
