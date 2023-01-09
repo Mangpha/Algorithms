27db99bf8956afc1bc536f33fedd7511dab6e449
Mangpha <8350130@gmail.com>
2023-01-09 11:38:41 +0900

    Fix Update ReadMe

--- UpdateReadme.js
+++ UpdateReadme.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const root = "./";
 const exclude = [".git", "README.md", "UpdateReadme.js", ".github"];
 const folders = fs.readdirSync(root).filter((name) => !exclude.includes(name));
+const lanDict = {
+    js: "JavaScript",
+    py: "Python",
+};
 const sites = {
     백준: "https://www.acmicpc.net/problem/",
     프로그래머스: "https://school.programmers.co.kr/learn/courses/30/lessons/",
@@ -22,17 +26,25 @@ folders.forEach((folder) => {
     const DifficultyLevels = fs.readdirSync(root + folder);
     markdown += `### ${folder}
     
-| Level | Prob ID | Title(Link) | Code |
-|:-----:|:-------:|:-----------:|:----:|
+| Level | Prob ID | Title(Link) | Code | Language |
+|:-----:|:-------:|:-----------:|:----:|:---------|
 `;
     DifficultyLevels.forEach((level) => {
-        const Probs = fs.readdirSync(root + folder + "/" + level);
+        const Probs = fs.readdirSync(`${root}/${folder}/${level}`);
         Probs.forEach((prob) => {
             const [id, title] = prob.split(/\.\s/);
-            markdown += `| ${level} | ${id} | [${title}](${
-                sites[folder] + id
-            }) | [Code](https://github.com/Mangpha/Algorithms/tree/master/${folder}/${level}/${prob}/${title}.js)
-`;
+            const filename = fs
+                .readdirSync(`${root}/${folder}/${level}/${prob}`)
+                .filter((name) => !exclude.includes(name));
+            filename.forEach((file) => {
+                const language = file.split(".")[1];
+                markdown += `| ${level} | ${id} | [${title}](${
+                    sites[folder] + id
+                }) | [Code](https://github.com/Mangpha/Algorithms/tree/master/${folder}/${level}/${prob}/${file} | ${
+                    lanDict[language]
+                })
+    `;
+            });
         });
     });
 
